93b43317c6062a8eda25c31325cb769442eb17ae
Yash N Ganji <97722390+YashNGanji@users.noreply.github.com>
2022-01-18 18:42:50 +0530

    Delete MUX2X1 directory

--- MUX2X1/mux2x1.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-module MUX2x1(I, s, y);
-    input [1:0]I ;//2 inputs 
-    input s;     //1 select line
-    output y;    //1 output
-        assign y = (s)? I[1]:I[0];
-endmodule
